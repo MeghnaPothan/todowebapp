9844172f657357cba940f056344cf53ec4f88c7e
Meghna <meghnapothan@gmail.com>
2022-08-02 20:08:05 -0400

    created views , designed  display  for the todo list and ad the todo list, added mySql. package, ,made a controller for databased operations .

---
+++ todo_webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace todo_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
