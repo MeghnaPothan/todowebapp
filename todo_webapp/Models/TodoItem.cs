7bd30cd9523e2d148f3473c2afec55984f6bc702
Meghna <meghnapothan@gmail.com>
2022-08-02 20:13:28 -0400

    added sqlite file

---
+++ todo_webapp/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+using System;
+
+//created a class for getting name and ID 
+namespace Todo.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
